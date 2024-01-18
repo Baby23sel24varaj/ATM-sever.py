b81eae749bab073833d3ad1bfbe6ba1547484d38
Baby23sel24varaj <147465199+Baby23sel24varaj@users.noreply.github.com>
2024-01-18 16:08:50 +0530

    Create ATMserver.py

---
+++ ATMserver.py
@@ -0,0 +1,21 @@
+card=""1234 5678 9034 5678"
+pin="1234"
+amount=1000
+amount_start=0
+for W in(1,);
+print("Welcome to HRK bank")
+print("insert your card")
+if (card="1234 5678 9034 5678" and pin="1234"):
+print("Enter your pin number :Success")
+while amount_start <=amount:
+  print("ATM ok done",amount_start)
+  amount_start+=100
+  print("please collect your cash ",amount)
+  elif(card!="1234 5678 9034 5678" and pin!="1234"):
+   print("Both pin and ATM card is wrong")
+  elif(card="1234 5678 9034 5678" or pin!="1234"):
+   print("anyone pin or ATM card is wrong")
+else:
+print ("Both pin and ATM card is wrong")
+amount_start=0
+ 
